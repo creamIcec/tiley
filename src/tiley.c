654ca508a6edc8f2f972e62d694dc1bf04c9d99d
Iriseplosc <42510470+creamIcec@users.noreply.github.com>
2025-05-30 00:15:31 +0800

    init:初始化仓库

---
+++ src/tiley.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define PROJECT_NAME "tiley"
+
+int main(int argc, char* argv[]){
+    printf("我们的桌面管理器从这里开始!\n");
+    return 0;
+}
---
+++ src/tiley.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#define PROJECT_NAME "tiley"
+
+int main(int argc, const char* argv[]){
+    std::cout << "我们的桌面管理器从这里开始!" << std::endl;
+}
